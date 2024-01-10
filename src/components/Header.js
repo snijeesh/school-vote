1d675adc517e21af9c40cba3d847f2f87bb7c94b
Snijeesh <spalott@starbucks.com>
2024-01-10 12:05:45 -0800

    Updated header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from '../logo.svg';
+import logo from '../school.jpg';
 
 function Header() {
     return (
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-            Edit <code>src/App.js</code>.
-            </p>
         </header>
     )
 }
