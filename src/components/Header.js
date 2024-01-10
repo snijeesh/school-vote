6d3657dcb622ab1822acf80fc1ba9be46ec48647
Snijeesh <spalott@starbucks.com>
2024-01-10 11:22:44 -0800

    Created header component.

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+function Header() {
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+            Edit <code>src/App.js</code>.
+            </p>
+        </header>
+    )
+}
+
+export default Header;
