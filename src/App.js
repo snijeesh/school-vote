b8cfbfaea526a4432d9b6b56c53f560dacfae0aa
Snijeesh <spalott@starbucks.com>
2024-01-30 13:47:12 -0800

    Added routing.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
 import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Login />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/home" Component={Home} />
+          <Route path="/login" Component={Login} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
