cdc75805ec629053ce47ce946a2b6763b83feb12
Snijeesh <spalott@starbucks.com>
2024-02-01 16:14:10 -0800

    Used Link for menu.

--- src/components/admin/Menu.js
+++ src/components/admin/Menu.js
@@ -1,31 +1,20 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import useAuthContext from "../auth/UseAuthContext";
 
 function Menu() {
 
     const { userToken } = useAuthContext();
-    const navigate = useNavigate();
     if(!userToken) {
         return (
             <div></div>
         )
     }
 
-    const menuOnClick = (event) => {
-        const {id} = event.target;
-        if(id === 'home') {
-            navigate("/admin/home");
-        }
-        if(id === 'register') {
-            navigate("/admin/register");
-        }
-    }
-
     return (
         <div className='menu'>
-            <a id='home' onClick={ menuOnClick }>Home</a>|
-            <a id='register' onClick={ menuOnClick }>Register</a>|
+            <Link to="/admin/home">Home</Link>
+            <Link to="/admin/register">Register</Link>
         </div>
     );
 }
