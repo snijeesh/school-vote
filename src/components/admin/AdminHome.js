4f6472d8e2e4073d1c9b58484dc990e8b660f743
Snijeesh <spalott@starbucks.com>
2024-02-05 14:37:20 -0800

    Added buttons.

--- src/components/admin/AdminHome.js
+++ src/components/admin/AdminHome.js
@@ -1,15 +1,28 @@
 import { Navigate } from "react-router-dom";
 import useAuthContext from "../auth/UseAuthContext";
 
+
 const AdminHome = () => {
     const { userInfo } = useAuthContext();
     if (!userInfo || userInfo?.role !== 'admin') {
         console.log('not logged in.');
         return <Navigate replace to="/login" />;
     }
+
+    const handleOnClick = () => {
+        let activateVoting = {
+            isActivateVoting: true
+        }
+        console.log('activated');
+    }
     
     return (
-        <div>Admin home page..</div>
+        <div>
+            <div className="page-header">
+                Admin home
+            </div>
+            <button onClick={handleOnClick} >Activate Vote</button>
+        </div>
     );
 };
 
--- src/components/user/UserHome.js
+++ src/components/user/UserHome.js
@@ -1,15 +1,38 @@
+import { useState, useEffect } from 'react';
 import { Navigate } from "react-router-dom";
 import useAuthContext from "../auth/UseAuthContext";
 
 const UserHome = () => {
     const { userInfo } = useAuthContext();
+    //const [activateVoting, setActivateVoting] = useState({});
+
+    useEffect(() => {
+        console.log("use effect running..");
+        // setActivateVoting({
+        //     isActivateVoting: false
+        // }, {});
+    });
+
     if (!userInfo && userInfo?.role !== 'user') {
         console.log('not logged in.');
         return <Navigate replace to="/login" />;
     }
     
+
+    
+    
+    const handleOnClick = () => {
+        
+        console.log('voted');
+    }
+
     return (
-        <div>User home page..</div>
+        <div>
+            <div className="page-header">User home</div>
+            <div>
+                <button onClick={handleOnClick} >Start Vote</button>
+            </div>        
+        </div>
     );
 };
 
