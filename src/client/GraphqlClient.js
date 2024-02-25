1cbdf9e1b4fe1df194398191c5f88987709d105a
snijeesh <snijeesh@gmail.com>
2024-02-25 00:02:21 -0800

    Added api call for login.

--- src/client/GraphqlClient.js
+++ src/client/GraphqlClient.js
@@ -1,28 +1,62 @@
-import React, { Component } from 'react';
-//import {gql, useQuery} from '@apollo/client';
-
-// const query = gql`{
-//     login
-// }`;
-
 class GraphqlClient {
     
     login(userName, password) {
-        const users = [
-            {userName: 'admin', password: 'password', name: 'Admin', role: 'admin', token: 'testtoken'},
-            {userName: 'user', password: 'password', name: 'User', role: 'user', token: 'testtoken'}
-        ];
-        var currentUser = users.find((user) => user.userName === userName && user.password === password);
+        this.callLoginApi().then(data => {
+            console.log('data: ' + data.login);
+            
+            var currentUser = data.login.find((user) => user.userName === userName && user.password === password);
+            console.log(currentUser);
 
-        if(currentUser) {
-            return {name: currentUser.name, role: currentUser.role, token: currentUser.token};
-        }
+            if(currentUser) {
+                return {name: currentUser.name, role: currentUser.role, token: currentUser.token};
+            }
+    
+            return {}
+        });
+        
+        
+        // const users = [
+        //     {userName: 'admin', password: 'password', name: 'Admin', role: 'admin', token: 'testtoken'},
+        //     {userName: 'user', password: 'password', name: 'User', role: 'user', token: 'testtoken'}
+        // ];
+        // var currentUser = users.find((user) => user.userName === userName && user.password === password);
+        // console.log(currentUser);
+        // if(currentUser) {
+        //     return {name: currentUser.name, role: currentUser.role, token: currentUser.token};
+        // }
 
-        return {}
+        // return {}
     }
+
     validateUser() {
         console.log('validateUser');
         return "testuser";
+    }
+
+    callLoginApi(callback) {
+        console.log('login started');
+        return fetch('http://localhost:8080/graphql', {
+            method: 'post',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                query: `
+                query LoginQuery{
+                    login {
+                        userName
+                        password
+                    }
+                }
+                `,
+            }),
+        })
+        .then((response => response.json()))
+        .then(data => {
+            console.log(data.data);
+            return data.data;
+        });
     }
 }
 
